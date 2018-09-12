c57f492973bd4a70e138c45b7d991e1e91bfe187
cnt0 <sarum9n@gmail.com>
2018-09-12 12:18:50 +0500

    remove unnecessary dbus calls for iwd

--- src/blocks/iwd/mod.rs
+++ src/blocks/iwd/mod.rs
@@ -1,4 +1,6 @@
 use chan::Sender;
+use std::default::Default;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{Duration, Instant};
 
@@ -10,7 +12,7 @@ use widget::{I3BarWidget, State};
 use widgets::text::TextWidget;
 
 use self::stdintf::org_freedesktop_dbus::PropertiesPropertiesChanged as PropsChanged;
-use blocks::dbus::{stdintf, BusType, Connection, ConnectionItem, SignalArgs};
+use blocks::dbus::{arg::RefArg, stdintf, BusType, Connection, ConnectionItem, Path, SignalArgs};
 use uuid::Uuid;
 
 mod net_connman_iwd_device;
@@ -19,11 +21,13 @@ use self::net_connman_iwd_device::NetConnmanIwdDevice;
 use self::net_connman_iwd_network::NetConnmanIwdNetwork;
 
 const IWD_IFACE: &str = "net.connman.iwd";
-const DEVICE_IFACE: &str = "net.connman.iwd.Device";
 
 const STATE_CONNECTED: &str = "connected";
 const STATE_DISCONNECTED: &str = "disconnected";
 const STATE_DISCONNECTING: &str = "disconnecting";
+
+const CHANGE_NETWORK: &str = "ConnectedNetwork";
+const CHANGE_STATE: &str = "State";
 
 const TIMEOUT: i32 = 100000;
 
@@ -37,9 +41,15 @@ fn get_widget_state(state: &str) -> State {
 
 pub struct IWD {
     id: String,
-    device_id: String,
     network: TextWidget,
+    cur_state: Arc<Mutex<IWDPrivate>>,
     dbus_conn: Connection,
+}
+
+#[derive(Default, Debug)]
+struct IWDPrivate {
+    network_obj: String,
+    state: String,
 }
 
 #[derive(Deserialize, Debug, Default, Clone)]
@@ -57,21 +67,39 @@ impl ConfigBlock for IWD {
     fn new(block_config: Self::Config, config: Config, send: Sender<Task>) -> Result<Self> {
         let id: String = Uuid::new_v4().simple().to_string();
         let id_copy = id.clone();
+        let cur_state: Arc<Mutex<IWDPrivate>> = Arc::new(Mutex::new(Default::default()));
+        let cur_state_copy = cur_state.clone();
 
         thread::spawn(move || {
             let c = Connection::get_private(BusType::System).unwrap();
-            c.add_match(&PropsChanged::match_str(Some(&IWD_IFACE.into()), None))
-                .unwrap();
+            let device_id_copy = block_config.device_id.clone();
+            {
+                let state = &mut *cur_state.lock().unwrap();
+                let device = c.with_path(IWD_IFACE, device_id_copy, TIMEOUT);
+                state.state = device.get_state().unwrap();
+                if state.state == STATE_CONNECTED {
+                    state.network_obj = device.get_connected_network().unwrap().to_string();
+                }
+            }
+            c.add_match(&PropsChanged::match_str(
+                Some(&IWD_IFACE.into()),
+                Some(&Path::from(block_config.device_id)),
+            )).unwrap();
             loop {
                 for ci in c.iter(TIMEOUT) {
                     if let ConnectionItem::Signal(msg) = ci {
                         if let Some(props) = PropsChanged::from_message(&msg) {
-                            if props.interface_name == DEVICE_IFACE {
-                                send.send(Task {
-                                    id: id.clone(),
-                                    update_time: Instant::now(),
-                                });
+                            let state = &mut *cur_state.lock().unwrap();
+                            if let Some(new_obj) = props.changed_properties.get(CHANGE_NETWORK) {
+                                state.network_obj = new_obj.as_str().unwrap().to_string();
                             }
+                            if let Some(new_state) = props.changed_properties.get(CHANGE_STATE) {
+                                state.state = new_state.as_str().unwrap().to_string();
+                            }
+                            send.send(Task {
+                                id: id.clone(),
+                                update_time: Instant::now(),
+                            });
                         }
                     }
                 }
@@ -80,7 +108,7 @@ impl ConfigBlock for IWD {
 
         Ok(IWD {
             id: id_copy,
-            device_id: block_config.device_id,
+            cur_state: cur_state_copy,
             network: TextWidget::new(config.clone())
                 .with_icon("wifi")
                 .with_state(State::Critical)
@@ -97,18 +125,15 @@ impl Block for IWD {
     }
 
     fn update(&mut self) -> Result<Option<Duration>> {
-        let device = self
-            .dbus_conn
-            .with_path(IWD_IFACE, self.device_id.clone(), TIMEOUT);
-
-        let cur_state = device.get_state().unwrap();
-        self.network.set_state(get_widget_state(cur_state.as_str()));
-        self.network.set_text(match cur_state.as_str() {
+        let cur_state = &mut *self.cur_state.lock().unwrap();
+        self.network
+            .set_state(get_widget_state(cur_state.state.as_str()));
+        self.network.set_text(match cur_state.state.as_str() {
             STATE_DISCONNECTED => STATE_DISCONNECTED.to_owned(),
             STATE_DISCONNECTING => STATE_DISCONNECTING.to_owned(),
             _ => NetConnmanIwdNetwork::get_name(&self.dbus_conn.with_path(
                 IWD_IFACE,
-                device.get_connected_network().unwrap(),
+                cur_state.network_obj.as_str(),
                 TIMEOUT,
             )).unwrap(),
         });
